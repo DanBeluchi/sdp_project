1e70a4db457ad5bde010f15918c77386ebde630b
Silvio Vl <io_prat@outlook.com>
2022-12-15 19:26:48 +0100

    formatting

--- sensor_api/disk.py
+++ sensor_api/disk.py
@@ -3,13 +3,14 @@ from gpiozero import DiskUsage
 
 disk = Blueprint("disk", __name__, url_prefix="/disk")
 
+
 @disk.route("/usage")
 def disk_usage():
     """
     This function provides disk usage information of the root partition.
     Returns usage in percent
     """
-    
-    disk = DiskUsage()
-    
-    return 'Current disk usage: {}%'.format(disk.usage)
+
+    disk_objc = DiskUsage()
+
+    return 'Current disk usage: {}%'.format(disk_objc.usage)
