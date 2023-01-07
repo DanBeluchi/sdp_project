6aea38e0b7296f858f3d76a9f925a3796f17b1c7
Silvio Vl <io_prat@outlook.com>
2023-01-07 13:03:31 +0100

    add test markers

--- tests/test_unit_disk.py
+++ tests/test_unit_disk.py
@@ -9,12 +9,14 @@ sys.path.insert(1, './sensor_api')  # nopep8
 # yapf: enable
 
 # pylint: disable=C0413
+import pytest
 # pylint: disable=E0401
 import disk  # type: ignore
 # pylint: enable=E0401
 # pylint: enable=C0413
 
 
+@pytest.mark.unittest
 def test_disk_usage_not_null():
     """
     Test that return object is not null.
@@ -23,6 +25,7 @@ def test_disk_usage_not_null():
     assert value != 0
 
 
+@pytest.mark.unittest
 def test_disk_usage_str_not_empty():
     """
     Test that return object is not empty.
