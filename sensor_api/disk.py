c94a727f9a53f57a7ead885cbf66873aed8d7e9a
Silvio Vl <io_prat@outlook.com>
2023-01-07 10:52:14 +0100

    implement endpoint 3

--- sensor_api/disk.py
+++ sensor_api/disk.py
@@ -1,16 +1,27 @@
+"""
+Shutil lib used to get disk information.
+"""
+import shutil
 from flask import Blueprint
-from gpiozero import DiskUsage
 
 disk = Blueprint("disk", __name__, url_prefix="/disk")
 
 
-@disk.route("/usage")
-def disk_usage():
+def disk_usage_in_percent():
     """
     This function provides disk usage information of the root partition.
-    Returns usage in percent
+    Returns usage in percent.
+    """
+    disk_object = shutil.disk_usage("/")
+
+    return (100 / disk_object.free) * disk_object.used
+
+
+@disk.route("/usage")
+def disk_usage_str():
+    """
+    This function provides disk usage information of the root partition.
+    Returns usage in percent of type string.
     """
 
-    disk_objc = DiskUsage()
-
-    return 'Current disk usage: {}%'.format(disk_objc.usage)
+    return f"{disk_usage_in_percent():0.2f}%"
