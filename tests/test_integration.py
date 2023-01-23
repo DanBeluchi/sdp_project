a147222ceea3df151a3710c8c04e28d2ae3226dc
Silvio Vl <io_prat@outlook.com>
2023-01-23 21:24:16 +0100

    integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,8 +11,67 @@ sys.path.insert(1, './sensor_api')  # nopep8
 # pylint: disable=C0413
 import pytest
 # pylint: disable=E0401
-import disk  # type: ignore
+import flask_app  # type: ignore
 # pylint: enable=E0401
 # pylint: enable=C0413
 
-# to be done!
+
+@pytest.fixture()
+def app():
+    '''
+    Create app fixture.
+    '''
+
+    app_fixture = flask_app.create_app()
+    app_fixture.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app_fixture
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app_fixture):
+    '''
+    Return test client.
+    '''
+    return app_fixture.test_client()
+
+
+@pytest.fixture()
+def runner(app_fixture):
+    '''
+    Return test cli runner.
+    '''
+    return app_fixture.test_cli_runner()
+
+
+@pytest.mark.integrationtest
+def test_get_disk_usage_not_empty():
+    """
+    Test that return object is not null.
+    """
+    response = client.get("/disk/usage")
+    assert response != ""
+
+
+@pytest.mark.integrationtest
+def test_get_cpu_temp_not_empty():
+    """
+    Test that return object is not null.
+    """
+    response = client.get("/cpu/temp")
+    assert response != ""
+
+
+@pytest.mark.integrationtest
+def test_get_cpu_temp_error_not_empty():
+    """
+    Test that return object is not null.
+    """
+    response = client.get("/cpu/temp/error")
+    assert response != ""
